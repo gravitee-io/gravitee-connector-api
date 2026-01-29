48371bab7c102ccb50540229f78f06682c0fd1a0
Remi Baptiste <remi.baptiste@graviteesource.com>
2026-01-29 13:36:34 +0100

    feat: upgrade to Vert.x 5 BREAKING CHANGE: upgrade to Vert.x 5

--- src/main/java/io/gravitee/connector/api/AbstractConnector.java
+++ src/main/java/io/gravitee/connector/api/AbstractConnector.java
@@ -1,11 +1,11 @@
-/**
- * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +19,21 @@ import io.gravitee.common.component.AbstractLifecycleComponent;
 import io.gravitee.gateway.api.proxy.ProxyRequest;
 
 /**
+ * Abstract base implementation of the {@link Connector} interface.
+ *
+ * <p>This class provides a skeletal implementation of the {@link Connector} interface,
+ * extending {@link AbstractLifecycleComponent} to support lifecycle management.
+ * Concrete connector implementations should extend this class and implement the
+ * {@link Connector#request(io.gravitee.gateway.api.ExecutionContext, ProxyRequest, io.gravitee.gateway.api.handler.Handler)}
+ * method to handle proxy requests.</p>
+ *
+ * <p>By default, the lifecycle methods {@link #doStart()} and {@link #doStop()} are
+ * no-op implementations. Subclasses can override these methods to perform initialization
+ * and cleanup operations as needed.</p>
+ *
+ * @param <C> the type of connection this connector creates, must extend {@link Connection}
+ * @param <R> the type of proxy request this connector handles, must extend {@link ProxyRequest}
+ *
  * @author David BRASSELY (david.brassely at graviteesource.com)
  * @author GraviteeSource Team
  */
